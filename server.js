f639ad7cd10474bdc8caa7314c80f128871f45dd
jimh <james.l.heinecke@gmail.com>
2023-05-22 21:01:35 -0500

    updates to js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static('public'));
 // GET Routes
-app.get('/*', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 app.get('/notes', (req, res) =>
